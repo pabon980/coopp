48159ab1e8d66fea189ffdd8572f65bb9a15e7c8
pabon980 <pabon980as@gmail.com>
2026-02-16 19:00:59 -0500

    ...

---
+++ objproducto.java
@@ -0,0 +1,33 @@
+public class objproducto{
+    private String producto;
+    private int precio;
+    private int cantidad;
+
+    public objproducto() {
+    }
+
+    public String getProducto() {
+        return producto;
+    }
+
+    public void setProducto(String producto) {
+        this.producto = producto;
+    }
+
+    public int getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    }
